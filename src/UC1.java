ccecb76a1d80114d31d4a4b90bb64785e35e967c
NituRay24 <raynitu24@gmail.com>
2022-08-16 21:02:23 +0530

    Welcome to Gambling

---
+++ src/UC1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner.*;
+public class UC1 {
+    public void Gamblingsimulation()
+        {
+        int stack=100;
+        int bets=1;
+
+
+    }
+    public static void main(String[] args)
+    {
+        UC1 gmbl =new UC1();
+        System.out.println("WELCOME TO GAMBLING");
+
+    }
+}
